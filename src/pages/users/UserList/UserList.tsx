e4dbd42c4e675163368e17fe7a8cb6590ba7ce5a
ZajoncM <milosz.zajonc@gmail.com>
2022-10-04 13:30:31 +0200

    delete user

--- src/pages/users/UserList/UserList.tsx
+++ src/pages/users/UserList/UserList.tsx
@@ -1,23 +1,53 @@
-import { List } from "antd";
-import { useUsersQuery } from "../../../generated/graphql";
+import { DeleteOutlined } from "@ant-design/icons";
+import { Button, Space } from "antd";
+import Table, { ColumnsType } from "antd/lib/table";
+import {
+  User,
+  useRemoveUserMutation,
+  useUsersQuery,
+} from "../../../generated/graphql";
 
 const UserList = () => {
   const { data, loading } = useUsersQuery();
+  const [removeUser] = useRemoveUserMutation({ refetchQueries: ["users"] });
+  const columns: ColumnsType<User> = [
+    {
+      title: "Imię",
+      dataIndex: "firstName",
+      key: "firstName",
+    },
+    {
+      title: "Nazwisko",
+      dataIndex: "lastName",
+      key: "lastName",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
 
-  return (
-    <List
-      size="large"
-      bordered
-      split
-      loading={loading}
-      dataSource={data?.users}
-      renderItem={({ firstName, lastName }, index) => (
-        <List.Item>
-          {index + 1}. {firstName} {lastName}
-        </List.Item>
-      )}
-    />
-  );
+    {
+      title: "Action",
+      key: "action",
+      render: (_, user) => (
+        <Space size="middle">
+          <Button
+            danger
+            icon={<DeleteOutlined />}
+            onClick={() =>
+              removeUser({
+                variables: { id: Number(user.id) },
+              })
+            }
+          />
+        </Space>
+      ),
+    },
+  ];
+
+  // TODO: Resolve this error
+  return <Table columns={columns} loading={loading} dataSource={data?.users} />;
 };
 
 export default UserList;
