c0550b09830063d7ae0e859065dcb0e0f0c09d97
ZajoncM <milosz.zajonc@gmail.com>
2022-11-12 15:52:19 +0100

    finish

--- src/pages/dashboard/ParentOverall/ParentOverall.tsx
+++ src/pages/dashboard/ParentOverall/ParentOverall.tsx
@@ -1,12 +1,46 @@
-import { Card, Typography } from "antd";
+import { Card, Collapse, Divider, List, Typography } from "antd";
 import { useParentQuery } from "../../../generated/graphql";
 
+const { Panel } = Collapse;
+
 const ParentOverall = () => {
-  const { data } = useParentQuery();
-  console.log(data);
+  const { data, loading } = useParentQuery();
+
   return (
-    <Card>
+    <Card loading={loading}>
       <Typography.Title level={4}>Uczniowie</Typography.Title>
+      <Collapse>
+        {data?.parent.children.map((child) => (
+          <Panel
+            header={`${child.user.firstName} ${child.user.lastName}`}
+            key={child.id}
+          >
+            <List
+              size="small"
+              header={<div>Obecności</div>}
+              bordered
+              dataSource={child?.attendances}
+              renderItem={(attendance) => (
+                <List.Item>
+                  {attendance.lesson.name} - {attendance.type}
+                </List.Item>
+              )}
+            />
+            <Divider orientation="left" />
+            <List
+              size="small"
+              header={<div>Oceny</div>}
+              bordered
+              dataSource={child?.grades}
+              renderItem={(grade) => (
+                <List.Item>
+                  {grade.exam.name} - {grade.value}
+                </List.Item>
+              )}
+            />
+          </Panel>
+        ))}
+      </Collapse>
     </Card>
   );
 };
--- src/templates/DashboardTemplate/DashboardTemplate.tsx
+++ src/templates/DashboardTemplate/DashboardTemplate.tsx
@@ -3,11 +3,15 @@ import { Header, Content } from "antd/lib/layout/layout";
 import Sider from "antd/lib/layout/Sider";
 import { PropsWithChildren } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserRoleEnum } from "../../generated/graphql";
+import { useUserContext } from "../../providers/UserProvider";
 import NavMenu from "./NavMenu/NavMenu";
 
 type Props = PropsWithChildren;
 
 const DashboardTemplate = ({ children }: Props) => {
+  const user = useUserContext();
+
   const navigate = useNavigate();
 
   const logout = () => {
@@ -15,24 +19,29 @@ const DashboardTemplate = ({ children }: Props) => {
     navigate("/login");
   };
 
+  const isAdmin = user?.role === UserRoleEnum.Admin;
+
   return (
     <Layout hasSider>
-      <Sider
-        style={{
-          overflow: "auto",
-          height: "100vh",
-          position: "fixed",
-          left: 0,
-          top: 0,
-          bottom: 0,
-        }}
-        theme="light"
-      >
-        <NavMenu />
-      </Sider>
+      {isAdmin && (
+        <Sider
+          style={{
+            overflow: "auto",
+            height: "100vh",
+            position: "fixed",
+            left: 0,
+            top: 0,
+            bottom: 0,
+          }}
+          theme="light"
+        >
+          <NavMenu />
+        </Sider>
+      )}
+
       <Layout
         className="site-layout"
-        style={{ marginLeft: 200, minHeight: "100vh" }}
+        style={{ marginLeft: isAdmin ? 200 : 0, minHeight: "100vh" }}
       >
         <Header
           style={{
